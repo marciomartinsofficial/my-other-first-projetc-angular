277aa5c46a51c1119b13f88c485702b8dea2bcc8
marcio.martins@sefin.fortaleza.ce.gov.br <a.marcio.sm@gmail.com>
2023-01-09 07:53:28 -0300

    Life cycle - OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-other-first-projetc-angular';
+  
+  ngOnInit(): void {
+    console.log('Please, wait for two second...');
+    setTimeout( () => {
+      console.log('Go!!!');
+    }, 2000);
+  }
 }
