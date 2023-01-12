e7b426929f0ed1d555d8187bb21da12cc1406aa7
marcio.martins@sefin.fortaleza.ce.gov.br <a.marcio.sm@gmail.com>
2023-01-12 07:06:00 -0300

    Component data-binding-interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
+import { DataBindingInterpolationComponent } from './data-binding-interpolation/data-binding-interpolation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    DataBindingInterpolationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data-binding-interpolation/data-binding-interpolation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding-interpolation',
+  templateUrl: './data-binding-interpolation.component.html',
+  styleUrls: ['./data-binding-interpolation.component.scss']
+})
+export class DataBindingInterpolationComponent {
+
+}
