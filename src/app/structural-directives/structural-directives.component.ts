d5f55b75d9aa4693ffdbc5638a22de5d7281f544
marcio.martins@sefin.fortaleza.ce.gov.br <a.marcio.sm@gmail.com>
2023-01-16 21:37:44 -0300

    Structural directives - ngIf - practice

--- src/app/structural-directives/structural-directives.component.ts
+++ src/app/structural-directives/structural-directives.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-structural-directives',
   templateUrl: './structural-directives.component.html',
   styleUrls: ['./structural-directives.component.scss']
 })
-export class StructuralDirectivesComponent {
+export class StructuralDirectivesComponent implements OnInit {
+  
+  public printP:boolean = true;
+
+  ngOnInit(): void {
+    setInterval( () => {
+      this.printP = !this.printP;
+    }, 2000);
+  }
 
 }
